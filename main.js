cf2d33bd9879ca694022abd673224a94632b4a5a
Denver Rivera <dpr9853@gmail.com>
2023-09-05 23:47:45 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const colorContainer = document.querySelector('.color-container');
+const generateBtn = document.querySelector('.generate-btn');
+
+getColors();
+
+async function getColors () {
+    var url = "http://colormind.io/api/";
+    var data = {
+        model : "default"
+    }
+
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            var palette = JSON.parse(http.responseText).result;
+
+            const names = [];
+            const colorPaletteNames = document.createElement("ul");
+            colorPaletteNames.classList.add("palette-name");
+            for (let i = 0; i<=10; i++){
+                const colorPal = palette[i];
+                if (colorPal && colorPal !== "") {
+                    names.push(colorPal);
+                }
+            }
+            const paletteNamesList = names.map(colorPal => `
+                <li class="indiv-name">${colorPal}</li>
+            `).join("");
+            colorPaletteNames.innerHTML = `
+                ${paletteNamesList}
+            `
+            colorContainer.appendChild(colorPaletteNames);
+
+            const colorPalette = document.createElement("ul");
+            colorPalette.classList.add("palette-container");
+            const palettes = [];
+            for (let i = 0; i<=10; i++){
+                const colorPal = palette[i];
+                if (colorPal && colorPal !== "") {
+                    palettes.push(colorPal);
+                }
+            }
+            const palettesList = palettes.map(colorPal => `
+                <li class="indiv-color" style="background:rgb(${colorPal});"></li>
+            `).join("");
+            colorPalette.innerHTML = `
+                ${palettesList}
+            `
+            colorContainer.appendChild(colorPalette);
+        }
+    }
+
+    http.open("POST", url, true);
+    http.send(JSON.stringify(data));
+}
+
+function savePaletteLS (color) {
+
+}
+
+generateBtn.addEventListener("click", () => {
+    window.location.reload();
+})
